4aaa847676f2dfc25efec9c661f2030a50678fbc
benny111 <benny.myname@gmail.com>
2014-01-05 10:17:02 +0800

    close #2,#3

--- js/loader.js
+++ js/loader.js
@@ -5,18 +5,23 @@ window.addEventListener("load", function() {
             load : [
             	/*"js/news.js",*/
                 "js/jquery.min.js",
+                "js/jquery.tmpl.js",
                 "js/sizzle.js",
                 "js/bootstrap.js",
                 
 
             ],
             
-            //complete : function() {
+            complete : function() {
+                     var news = [{"photo":"2009.jpg","title":"天燈"},
+                            {"photo":"2009.jpg","title":"天燈"}];
+
+                            $('#tmpl-news').tmpl(news).appendTo( "#end" );
                /* $(cover).hover( function(){ alert("Ready....");});*/
             //    $(cover).hover(GameModule.test());
                 /*$(cover).click(EventHandler.click());*/
 
-            //}
+            }
         }
     ]);
 
